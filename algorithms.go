3cfa6d64ab00513684ab51039848042d0b5462a9
irenicaa <root@irenica.pro>
2020-08-13 17:52:27 +0300

    Fix the minimum function

--- algorithms.go
+++ algorithms.go
@@ -1,22 +1,10 @@
 package algorithms
 
-// MinimumInt ...
-func MinimumInt(items []int) int {
+// Minimum ...
+func Minimum(items []Less) Less {
 	minimum := items[0]
 	for _, item := range items[1:] {
-		if item < minimum {
-			minimum = item
-		}
-	}
-
-	return minimum
-}
-
-// MinimumFloat64 ...
-func MinimumFloat64(items []float64) float64 {
-	minimum := items[0]
-	for _, item := range items[1:] {
-		if item < minimum {
+		if item.Less(minimum) {
 			minimum = item
 		}
 	}
