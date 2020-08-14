a5492c0d9d48577de42282f45e73db08b20b3b1a
irenicaa <root@irenica.pro>
2020-08-14 23:59:30 +0300

    Implement tests of Minimum function

--- algorithms_test.go
+++ algorithms_test.go
@@ -21,7 +21,24 @@ func TestMinimum(t *testing.T) {
 		args args
 		want Less
 	}{
-		// TODO: Add test cases.
+		{
+			name: "succes",
+			args: args{
+				items: []Less{
+					IntLess(19732),
+					IntLess(13),
+					IntLess(4197),
+					IntLess(23711),
+					IntLess(23073),
+					IntLess(14740),
+					IntLess(22248),
+					IntLess(6874),
+					IntLess(6601),
+					IntLess(1608),
+				},
+			},
+			want: IntLess(13),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
