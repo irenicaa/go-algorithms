7591b1122c3351dc6e60359e7e63871647551ae9
irenicaa <root@irenica.pro>
2020-09-02 22:33:36 +0300

    Optimize the BubbleSort function

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -5,10 +5,15 @@ import algorithms "github.com/irenicaa/go-algorithms"
 // BubbleSort ...
 func BubbleSort(items []algorithms.Less) {
 	for j := range items {
+		wasSwapped := false
 		for i := 0; i < len(items)-1-j; i++ {
 			if items[i+1].Less(items[i]) {
 				items[i+1], items[i] = items[i], items[i+1]
+				wasSwapped = true
 			}
+		}
+		if !wasSwapped {
+			return
 		}
 	}
 }
