1bd139ec7f7db504035fc51d0a4170e5eb9acc21
irenicaa <root@irenica.pro>
2020-09-07 19:22:17 +0300

    Add the CocktailSort function

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -20,3 +20,33 @@ func BubbleSort(items []algorithms.Less) {
 		end = lastSwappedIndex
 	}
 }
+
+// CocktailSort ...
+func CocktailSort(items []algorithms.Less) {
+	start, end := 0, len(items)-1
+	for {
+		wasSwapped, lastSwappedIndex := false, 0
+		for i := start; i < end; i++ {
+			if items[i+1].Less(items[i]) {
+				items[i+1], items[i] = items[i], items[i+1]
+				wasSwapped, lastSwappedIndex = true, i
+			}
+		}
+		if !wasSwapped {
+			return
+		}
+		end = lastSwappedIndex
+
+		wasSwapped, lastSwappedIndex = false, 0
+		for i := end - 1; i >= start; i-- {
+			if items[i+1].Less(items[i]) {
+				items[i+1], items[i] = items[i], items[i+1]
+				wasSwapped, lastSwappedIndex = true, i
+			}
+		}
+		if !wasSwapped {
+			return
+		}
+		start = lastSwappedIndex
+	}
+}
