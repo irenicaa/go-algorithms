978e9cb143d841b74202c52175d4c13fa5e846c8
irenicaa <root@irenica.pro>
2020-09-13 17:50:09 +0300

    Add constants

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -2,12 +2,16 @@ package sorting
 
 import algorithms "github.com/irenicaa/go-algorithms"
 
+const wasNotSwapped = -1
+const forwardStep = 1
+const backwardStep = -1
+
 // BubbleSort ...
 func BubbleSort(items []algorithms.Less) {
 	end := len(items) - 1
 	for {
-		lastSwappedIndex := bubbleSortPass(items, 0, end, 1)
-		if lastSwappedIndex == -1 {
+		lastSwappedIndex := bubbleSortPass(items, 0, end, forwardStep)
+		if lastSwappedIndex == wasNotSwapped {
 			return
 		}
 
@@ -19,14 +23,14 @@ func BubbleSort(items []algorithms.Less) {
 func CocktailSort(items []algorithms.Less) {
 	start, end := 0, len(items)-1
 	for {
-		lastSwappedIndex := bubbleSortPass(items, start, end, 1)
-		if lastSwappedIndex == -1 {
+		lastSwappedIndex := bubbleSortPass(items, start, end, forwardStep)
+		if lastSwappedIndex == wasNotSwapped {
 			return
 		}
 		end = lastSwappedIndex
 
-		lastSwappedIndex = bubbleSortPass(items, end-1, start-1, -1)
-		if lastSwappedIndex == -1 {
+		lastSwappedIndex = bubbleSortPass(items, end-1, start-1, backwardStep)
+		if lastSwappedIndex == wasNotSwapped {
 			return
 		}
 		start = lastSwappedIndex
@@ -35,10 +39,10 @@ func CocktailSort(items []algorithms.Less) {
 
 func bubbleSortPass(items []algorithms.Less, start int, end int, step int) int {
 	if len(items) == 0 {
-		return -1
+		return wasNotSwapped
 	}
 
-	lastSwappedIndex := -1
+	lastSwappedIndex := wasNotSwapped
 	for i := start; i != end; i += step {
 		if items[i+1].Less(items[i]) {
 			items[i+1], items[i] = items[i], items[i+1]
