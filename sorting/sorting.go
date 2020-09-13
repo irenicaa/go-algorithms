5f3144982850e80ef697f3e9d42fbe50fe89f28f
irenicaa <root@irenica.pro>
2020-09-13 17:38:12 +0300

    Add the bubbleSortPass function

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -6,13 +6,7 @@ import algorithms "github.com/irenicaa/go-algorithms"
 func BubbleSort(items []algorithms.Less) {
 	end := len(items) - 1
 	for {
-		wasSwapped, lastSwappedIndex := false, 0
-		for i := 0; i < end; i++ {
-			if items[i+1].Less(items[i]) {
-				items[i+1], items[i] = items[i], items[i+1]
-				wasSwapped, lastSwappedIndex = true, i
-			}
-		}
+		wasSwapped, lastSwappedIndex := bubbleSortPass(items, 0, end, 1)
 		if !wasSwapped {
 			return
 		}
@@ -25,28 +19,32 @@ func BubbleSort(items []algorithms.Less) {
 func CocktailSort(items []algorithms.Less) {
 	start, end := 0, len(items)-1
 	for {
-		wasSwapped, lastSwappedIndex := false, 0
-		for i := start; i < end; i++ {
-			if items[i+1].Less(items[i]) {
-				items[i+1], items[i] = items[i], items[i+1]
-				wasSwapped, lastSwappedIndex = true, i
-			}
-		}
+		wasSwapped, lastSwappedIndex := bubbleSortPass(items, start, end, 1)
 		if !wasSwapped {
 			return
 		}
 		end = lastSwappedIndex
 
-		wasSwapped, lastSwappedIndex = false, 0
-		for i := end - 1; i >= start; i-- {
-			if items[i+1].Less(items[i]) {
-				items[i+1], items[i] = items[i], items[i+1]
-				wasSwapped, lastSwappedIndex = true, i
-			}
-		}
+		wasSwapped, lastSwappedIndex = bubbleSortPass(items, end-1, start-1, -1)
 		if !wasSwapped {
 			return
 		}
 		start = lastSwappedIndex
 	}
+}
+
+func bubbleSortPass(items []algorithms.Less, start int, end int, step int) (bool, int) {
+	if len(items) == 0 {
+		return false, 0
+	}
+
+	wasSwapped, lastSwappedIndex := false, 0
+	for i := start; i != end; i += step {
+		if items[i+1].Less(items[i]) {
+			items[i+1], items[i] = items[i], items[i+1]
+			wasSwapped, lastSwappedIndex = true, i
+		}
+	}
+
+	return wasSwapped, lastSwappedIndex
 }
